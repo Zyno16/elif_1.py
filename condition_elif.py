c088b3c17167d740a54472c42d3f60844c53f941
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-04 10:25:02 +0000

    Add files via upload

---
+++ condition_elif.py
@@ -0,0 +1,12 @@
+y = input("enter you grade ? ")
+
+print ("the number is ",y)
+if y == 'a':
+    print ("grade is distinction")  
+elif y == 'b':
+    print ("grade is melit")
+elif y == 'c':
+    print ("you pass")
+else:
+    print("you fail !!")
+    
